59475bf93dc17ed7b288f196686d8b5e7bb85b0e
Casey Wood <caseywood252@gmail.com>
2021-06-07 22:32:06 +0000

    updated with ejs view engine in server

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080 ;
 
+app.set('view engine', 'ejs')
+
+// json object 
 const urlDatabase = { 
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 app.get('/hello', (req, res) => {
   res.end('<html><body>Hello <b>World</b></body></html>\n')
 })
