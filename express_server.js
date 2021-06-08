fd5e974bfe167f7ec78cfe1c7ab55f894973005d
Casey Wood <caseywood252@gmail.com>
2021-06-08 00:39:37 +0000

    updated next page from urls, including single short and long

--- express_server.js
+++ express_server.js
@@ -10,12 +10,24 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
   }
   res.render('urls_index', templateVars)
 })
+
+app.get('/urls/:shortURL', (req, res) => {
+  console.log(req.params)
+  console.log()
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars)
+})
+
+
+
 
 app.get('/hello', (req, res) => {
   res.end('<html><body>Hello <b>World</b></body></html>\n')
