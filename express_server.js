5f5d8a032cfd5fe6139b23cf40585226184f2075
Casey Wood <caseywood252@gmail.com>
2021-06-08 21:54:18 +0000

    fixed link on newURL page to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -1,65 +1,62 @@
 const bodyParser = require("body-parser");
-
 const express = require('express');
 const app = express();
 const PORT = 8080 ;
 app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-// json object 
+// DATABASE
 const urlDatabase = { 
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// DATABASE
 
+// MY URLS FUNCTIONALITY-------------
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
   }
-  res.render('urls_index', templateVars)
-})
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
-})
-
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new')
+  res.render('urls_index', templateVars);
 })
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log(req.params.shortURL);
-  delete urlDatabase[req.params.shortURL]
-  console.log(urlDatabase)
-  res.redirect('/urls')
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 });
+// END OF MY URLS-------------------
+
+// NEW URLS ------------------------
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  let randoSmall = generateRandomString()
-  urlDatabase[randoSmall] = req.body.longURL 
-  console.log(urlDatabase)
-  res.redirect(`/urls/${randoSmall}`)  
+  let randoSmall = generateRandomString();
+  urlDatabase[randoSmall] = req.body.longURL;
+  res.redirect(`/urls/${randoSmall}`);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars)
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render('urls_show', templateVars);
 })
 
+// redirect from created page
 app.get("/u/:shortURL", (req, res) => {
-  // console.log(req.params.shortURL)
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(`http://${longURL}`);
 });
+// NEW URLS END------------------------
+
 
 function generateRandomString() {
-  return Math.random().toString(36).substr(2, 5)
-}
+  return Math.random().toString(36).substr(2, 5);
+};
 
 app.listen(PORT, () => {
-  console.log(`connected on port: ${PORT}!`)
-})
+  console.log(`connected on port: ${PORT}!`);
+});
 
 // app.get('/hello', (req, res) => {
 //   res.end('<html><body>Hello <b>World</b></body></html>\n')
@@ -68,3 +65,7 @@ app.listen(PORT, () => {
 // app.get('/', (req, res) => {
 //   res.send('Hello!\nYou can access other pages through , /urls.json, /hello, /urls')
 // });
+
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase)
+// })
