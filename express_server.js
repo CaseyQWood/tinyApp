7cebfefaf19e8e34bc0b10829e105d999a3be208
Casey Wood <caseywood252@gmail.com>
2021-06-08 01:53:52 +0000

    added randomizer for short URL, add .post to take in body from user

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+const bodyParser = require("body-parser");
+
 const express = require('express');
 const app = express();
 const PORT = 8080 ;
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
 // json object 
@@ -10,8 +12,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
-
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase
@@ -19,26 +19,37 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars)
 })
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars)
-})
-
-
-
-
 app.get('/hello', (req, res) => {
   res.end('<html><body>Hello <b>World</b></body></html>\n')
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello!\nYou can access other pages through , /urls.json, /hello')
+  res.send('Hello!\nYou can access other pages through , /urls.json, /hello, /urls')
 });
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars)
+})
+
+function generateRandomString() {
+  Math.random().toString(36).substr(2, 5)
+}
 
 app.listen(PORT, () => {
   console.log(`connected on port: ${PORT}!`)
 })
 
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
-})
